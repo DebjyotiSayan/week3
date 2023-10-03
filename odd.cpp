c2b047fc3efae24f82fb55b5375b8db550c53d64
DebjyotiSayan <143388775+DebjyotiSayan@users.noreply.github.com>
2023-10-03 13:32:54 +0530

    Add files via upload

---
+++ odd.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int n,i;
+
+int main(){
+	printf("Enter the value of n:");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++){
+		if(i%2!=0){
+			printf("%d\n",i);
+		}
+		
+	}
+	return 0;
+	
+}
